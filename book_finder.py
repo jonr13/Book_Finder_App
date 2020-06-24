a6e2fb41b054c6f66d95f6d7def5ba96dac2d734
jonr13 <65420529+jonr13@users.noreply.github.com>
2020-06-24 10:33:53 -0400

    Reduced Code Complexity after Code Climte Review

--- book_finder.py
+++ book_finder.py
@@ -49,48 +49,37 @@ for item in nyt_list['results']:
     readable_date = read_date(last_date)
     nyt_list_info[item['list_name']] = f"The best seller list for {list_name} was last updated on {readable_date} and is typically updated {last_updated.lower()}."
 
-
 #Print welcoming message to user with a description of the app
-print("               ")
-print("Welcome to the Book Finder!\nIn this app you'll be able to search for a book by genre, browse best seller lists, and book purchasing options.\nAll in one place." )
+welcome_message = "\nWelcome to the Book Finder!\nIn this app you'll be able to search for a book by genre, browse best seller lists, and book purchasing options.\nAll in one place."
+print(welcome_message)
 #For this app you can indicate book genre, and we'll pull best selling from the NYT
-print("               ")
-prompt = "When you're ready to being, press enter.."
+prompt = "\nWhen you're ready to begin press enter.."
 begin = input(prompt)
-print("               ")
-print("Let's browse New York Times Best Seller Lists to find a book!")
-print("               ")
-
-
+welcome_message2 = "\nLet's browse New York Times Best Seller Lists to find a book!\n"
+print(welcome_message2)
 #Allow the user to see what genres are available
 #Prompt the user for book genre
 #Print thank you, and print the genre selected
-genre = ''
 def list_browse():
     global genre
     global nyt_list_data_adj
     while True:
-        print("               ")
-        prompt1 = "Enter in the genre of best seller list you want to browse (ex. 'Hardcover Fiction').\nIf you want to see a complete list of genres, enter 'show me'.\nEnter here: "
+        prompt1 = "\nEnter in the genre of best seller list you want to browse (ex. 'Hardcover Fiction').\nIf you want to see a complete list of genres, enter 'show me'.\nEnter here: "
         genre = input(prompt1)
-        if genre == 'show me' or genre == 'Show me' or genre == 'SHOW ME':
+        if genre == 'show me':
             for lis in nyt_list_names:
                 print(lis)
         elif genre in nyt_list_names:
-            print("               ")
-            print("We found a matching genre!")
-            print(nyt_list_info[genre])
-            print("        ")
-            print(f"The New York Times Top 5 Best Selling {genre} Books: ")
-            print("        ")
+            matching_genre = f"\nWe found a matching genre!\n{nyt_list_info[genre]}\n\nThe New York Times Top 5 Best Selling {genre} Books: \n"
+            print(matching_genre)
             url_list = nyt_list_dict[genre]
             list_data_url = f"https://api.nytimes.com/svc/books/v3/lists/current/{url_list}.json?api-key=za7DPypRNtsNzAW8VGweJEJW6EHJJZSG"
             nyt_list_data = parse_json(list_data_url)
             nyt_list_data_adj = nyt_list_data['results']['books']
             break
         else:
-            print("               ")
-            print("We couldn't find a matching genre! Please try again")
+            no_match = "   \nWe couldn't find a matching genre! Please try again"
+            print(no_match)
 
 list_browse()
 
@@ -105,9 +94,8 @@ def add_books_to_list():
         prompt7 = "Enter the author here: "
         author_add = input(prompt7)
         read_list[book_add] = author_add
-        print("     ")
-        print(f"{book_add}, written by {author_add} has been added to the Read List!")
-        print("     ")
+        add_statement = f"\n{book_add}, written by {author_add} has been added to the Read List!\n"
+        print(add_statement)
 
 def book_ranking():
     for book in nyt_list_data_adj:
@@ -118,8 +106,8 @@ def book_ranking():
             book_name = book['title']
             author = book['author']
             book_list[book_name] = author
-            print(f"{rank}. {book_name}\nWritten by {author} ")
-            print("      ")
+            rank_statement = f"{rank}. {book_name}\nWritten by {author}\n"
+            print(rank_statement)
         else:
             pass
 
@@ -130,105 +118,73 @@ def book_description():
     for book in nyt_list_data_adj:
         if browse_or_read == book['title']:
             desc = book['description']
-            print("      ")
-            print(f"DESCRIPTON: {desc}")
-            print("      ")
+            description = f"\nDESCRIPTON: {desc}\n"
+            print(description)
         else:
             pass
 
 def more_options():
     while True:
         global browse_or_read
-        print("More Actions Below:")
-        print("       ")
-        prompt2 = "1. Would you like to see the description of a book to learn more? If so, enter the title of the book (ex. 'SULLY').\n2. Do you want to browse other book lists? If so, enter 'browse'.\n3. Do you want to add a book to your read list? If so, enter 'add'.\n \nIf you want to continue to Search on Amazon, type 'pass'.\n \nEnter here: "
+        prompt2 = "\nMore Actions Below:\n\n1. Would you like to see the description of a book to learn more? If so, enter the title of the book (ex. 'SULLY').\n2. Do you want to browse other book lists? If so, enter 'browse'.\n3. Do you want to add a book to your read list? If so, enter 'add'.\n \nIf you want to continue to Search on Amazon, type 'pass'.\n \nEnter here: "
         browse_or_read = input(prompt2)
-        print("      ")
-        if browse_or_read =='browse' or browse_or_read == 'Browse' or browse_or_read == 'BROWSE':
+        if browse_or_read =='browse':
             list_browse()
             book_ranking()
-        elif browse_or_read == 'pass' or browse_or_read == 'pass' or browse_or_read == 'pass':
+        elif browse_or_read == 'pass':
             break
-        elif browse_or_read == 'add' or browse_or_read == 'Add' or browse_or_read == 'ADD':
+        elif browse_or_read == 'add':
             add_books_to_list()
         elif browse_or_read in book_list:
             book_description()
 
-def print_desc(get):
-    amz = get['search_results']
+def amazon_search_results(product_data):
+    amz = product_data['search_results']
+    print("\nTop 3 Amazon Search Results:")
     for a in amz:
-        try:
-            if a['position'] < 4:
-                title = a['title']
-                print("      ")
-                print(title)
-                link = "       Navigate to Link: "+ a['link']
-                rating = "Average Rating: "+ str(a['rating'])
-                print(rating)
-                kindle = ''
-                kindle_price = ''
-                paperback = ''
-                paperback_price = ''
-                price_name = [price['name'] for price in a['prices']]
-                prices = []
-                for price in a['prices']:
-                    if price['name'] == 'Kindle':
-                        kindle = "    Available for Kindle"
-                        kindle_price = "        Kindle Price: "+ price['raw']
-                        print(kindle)
-                        print(kindle_price)
-                    if price['name'] == 'Paperback':
-                        paperback = "    Available in Paperback"
-                        paperback_price = "       Paperback Price: "+ price['raw']
-                        print(paperback)
-                        print(paperback_price)
-                    if "Kindle" and "Paperback" not in price_name:
-                        prices.append(price)
-                    else:
-                        pass
-                try:
-                    print("    Price: " + prices[0]['raw'])
-                except IndexError:
-                    pass
-                if a['is_prime'] == True:
-                    is_prime = '    Amazon Prime Eligible'
-                    print(is_prime)
-                else:
-                    pass
-                print(link)
-                print("     ")
-        except KeyError:
-            print("    Prices not available.")
-            print(link)
+        if a['position'] < 4:
+            title = a['title']
+            link = a['link']
+            try: 
+                rating = a['rating']
+            except KeyError:
+                rating = 'Rating not available'
+            prime = ''
+            if a['is_prime'] == True:
+                prime += 'Yes'
+            else:
+                prime += 'No'
+            prices = {}
+            try:
+                for l in a['prices']:
+                    prices[l['name']] = l['raw']
+            except KeyError:
+                prices['Price Not Available'] = ''
+            book_information = f"\n{title}\nAverage Rating: {rating}\nPrime Eligible: {prime}\nNavigate to Link: {link}\n   Price List:"
+            print(book_information)
+            for i in prices:
+                price_statement = f"   {i.capitalize()} {prices[i]}"
+                print(price_statement)
 
 def search_books():
     while browse_or_read != 'done':
-        print("   ")
-        print("Search and Finish Options: ")
-        prompt3 = "1. Would you like to search any book title on Amazon.com? If so, enter 'search'.\n2. Would you like to finish searching books? If so, enter 'done'.\n3. Would you like to continue to browse? If so, enter 'browse'.\nEnter here: "
-        print("       ")
+        prompt3 = "\nSearch and Finish Options: \n1. Would you like to search any book title on Amazon.com? If so, enter 'search'.\n2. Would you like to finish searching books? If so, enter 'done'.\n3. Would you like to continue to browse? If so, enter 'browse'.\nEnter here: "
         global search_book
         search_book = input(prompt3)
-        print("       ")
-        if search_book == 'browse' or search_book == 'Browse' or search_book == "BROWSE":
+        if search_book == 'browse':
             list_browse()
             book_ranking()
             break
-        elif search_book == 'done' or search_book == 'Done' or search_book == 'DONE':
-            print("               ")
+        elif search_book == 'done':
             if read_list:
-                print("Read List:")
                 for read in read_list:
-                    print(f"{read}, written by {read_list[read]}.")
-                print("               ")
-                print("We're done finding books! Keep on reading!")
+                    print(f"\nRead List:\n{read}, written by {read_list[read]}.\nWe're done finding books! Keep on reading!\n")
                 break
             else:
-                print("               ")
-                print("We're done finding books! Keep on reading!")
+                print("\nWe're done finding books! Keep on reading!")
                 break
-        elif search_book == 'search' or search_book == 'Search' or search_book == 'SEARCH':
-            prompt5 = "Enter Book Title here: "
+        elif search_book == 'search':
+            prompt5 = "\nEnter Book Title here: "
             search_book_title = input(prompt5)
             prompt4 = "Enter Book Author here: "
             search_book_author = input(prompt4)
@@ -237,23 +193,21 @@ def search_books():
             search_term = f"{search_book_title}+{search_book_author}"
             amazon_url = f"https://api.rainforestapi.com/request?api_key={rainforest_api1}&type=search&amazon_domain=amazon.com&search_term={search_term}"
             get= get_products(amazon_url)
-            print_desc(get)
+            amazon_search_results(get)
             break
 
 def final_prompt():
-    if search_book == 'done' or search_book == 'Done' or search_book == 'DONE':
-        print("     ")
-        print("Thank you for using the book finder app! Have a great day!")
+    if search_book == 'done':
+        print("\nThank you for using the book finder app! Have a great day!")
         pass 
-    elif search_book != 'done' or search_book != 'Done' or search_book != 'DONE':
-        prompt_final = ("Would you like to continue to research books?\nIf so type 'yes', if no type 'done'.\nEnter here: ")
+    elif search_book != 'done':
+        prompt_final = ("\nWould you like to continue to research books?\nIf so type 'yes', if no type 'done'.\nEnter here: ")
         final = input(prompt_final)
-        if final == 'yes' or final == 'Yes' or final == 'YES':
+        if final == 'yes':
             more_options()
             search_books()
     else:
-        print("      ")
-        print("Thank you for using the book finder app! Have a great day!")
+        print("\nThank you for using the book finder app! Have a great day!\n")
 
 #Ask the user if he/she wants to see the book on amazon
 #From Amazon API, pull in price and kindle format data
@@ -262,7 +216,7 @@ def final_prompt():
 while True:
     more_options()
     search_books()
-    if search_book == 'done' or search_book == 'Done' or search_book == 'DONE':
+    if search_book == 'done':
         break
 
 final_prompt()
